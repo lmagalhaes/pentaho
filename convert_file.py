f499cf796b500b6782c192a9989859fe106c6dc2
Leandro Magalhaes <leandro.magalhaes@theiconic.com.au>
2020-04-14 13:17:36 +1000

    Update convert_file.py to use multiprocessing

--- convert_file.py
+++ convert_file.py
@@ -1,8 +1,9 @@
 import os
 from glob import glob
 import decimal
-import shutil
 import gzip
+from multiprocessing.pool import Pool
+from time import time
 
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -11,6 +12,8 @@ ATTRIBUTE_SEPARATOR = "|"
 ORIGINALS_PATH = os.path.join(ROOT_DIR, 'originals')
 ZIPPED_PATH = os.path.join(ROOT_DIR, 'zipped')
 PARSED_PATH = os.path.join(ROOT_DIR, 'parsed')
+# for is a good number because it reduces the file size by more than 60% while keeps the compressing time to almost 1/2
+GZIP_COMPRESSION_LEVEL = 4
 
 
 def is_number(s):
@@ -25,15 +28,16 @@ def extract_file_name(file_path):
     return os.path.basename(file_path)
 
 
-def compress_file(original):
-    gzip_filename = '{}.gz'.format(
-        extract_file_name(original)
-    )
-    print('Compressing file {}'.format(gzip_filename))
-    gzip_file_path = os.path.join(ZIPPED_PATH, gzip_filename)
-    with gzip.open(gzip_file_path, 'w', compresslevel=9) as gzip_file:
-        with open(original, 'rb') as original_file:
-            shutil.copyfileobj(original_file, gzip_file)
+def parse_file(filename):
+    with gzip.GzipFile(filename, 'r') as file_:
+        return Pool(os.cpu_count()).map(parse_line, file_.readlines())
+
+
+def parse_line(line):
+    return ATTRIBUTE_SEPARATOR.join(list(map(
+        lambda attr: str(round_score(attr, 10)) if is_number(attr) else attr,
+        line.decode().strip().split(ATTRIBUTE_SEPARATOR)
+    ))).encode()
 
 
 def round_score(score, decimal_points=8):
@@ -49,23 +53,20 @@ if __name__ == '__main__':
         ORIGINALS_PATH, ZIPPED_PATH, PARSED_PATH
     ]
 
-    files = glob(os.path.join(ORIGINALS_PATH, '*.csv'))
+    for folder in required_folders:
+        if not os.path.isdir(folder):
+            os.makedirs(folder)
+
+    files = glob(os.path.join(ORIGINALS_PATH, 'configs', '201805', '*.csv.gz'))
 
     for config_file in files:
+        print('Parsing file: {}'.format(config_file))
         filename = extract_file_name(config_file)
 
         parsed_file_path = os.path.join(PARSED_PATH, filename)
-
-        with open(config_file, 'r') as config:
-            print('Parsing file {}'.format(config_file))
-            with open(parsed_file_path, 'w') as parsed_file:
-                for line in config.readlines():
-
-                    line = ATTRIBUTE_SEPARATOR.join(list(map(
-                        lambda attr: str(round_score(attr, 10)) if is_number(attr) else attr,
-                        line.strip().split(ATTRIBUTE_SEPARATOR)
-                    )))
-
-                    parsed_file.write('{}{}'.format(line, os.linesep))
-            print("Parse complete.")
-        compress_file(parsed_file_path)
+        t1 = time()
+        with gzip.GzipFile(parsed_file_path, 'w', compresslevel=GZIP_COMPRESSION_LEVEL) as parsed_file:
+            parsed_lines = parse_file(config_file)
+            parsed_file.writelines(parsed_lines)
+        t2 = time()
+        print("Parse complete in {:.2f}s.".format(t2 - t1))
