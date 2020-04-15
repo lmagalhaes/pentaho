c8bc28a8cef0016d74dd1030f69d012b8015f7f5
Leandro Magalhaes <leandro.magalhaes@theiconic.com.au>
2020-04-15 18:27:42 +1000

    Create commands and remove duplication (still some to remove)

--- convert_file.py
+++ convert_file.py
@@ -1,9 +1,13 @@
-import os
-from glob import glob
 import decimal
+import glob
 import gzip
+import os
+from datetime import datetime
 from multiprocessing.pool import Pool
 from time import time
+
+import click
+from dateutil.rrule import rrule, MONTHLY
 
 
 ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -13,9 +17,9 @@ ORIGINALS_PATH = os.path.join(ROOT_DIR, 'originals')
 ZIPPED_PATH = os.path.join(ROOT_DIR, 'zipped')
 PARSED_PATH = os.path.join(ROOT_DIR, 'parsed')
 # for is a good number because it reduces the file size by more than 60% while keeps the compressing time to almost 1/2
-GZIP_COMPRESSION_LEVEL = 4
+GZIP_COMPRESSION_LEVEL = 3
 NUMBER_OF_WORKERS = os.cpu_count()
-
+DATE_FORMAT = '%Y%m'
 
 def is_number(s):
     try:
@@ -29,16 +33,18 @@ def extract_file_name(file_path):
     return os.path.basename(file_path)
 
 
-def parse_file(filename):
+def parse_file(filename, workers):
     with gzip.GzipFile(filename, 'r') as file_:
-        return Pool(processes=NUMBER_OF_WORKERS).map(parse_line, file_.readlines())
+        return Pool(processes=workers).map(parse_line, file_.readlines())
 
 
 def parse_line(line):
-    return ATTRIBUTE_SEPARATOR.join(list(map(
-        lambda attr: str(round_score(attr, 10)) if is_number(attr) else attr,
-        line.decode().strip().split(ATTRIBUTE_SEPARATOR)
-    ))).encode()
+    return '{}{}'.format(
+        ATTRIBUTE_SEPARATOR.join(list(map(
+            lambda attr: str(round_score(attr, 10)) if is_number(attr) else attr,
+            line.decode().strip().split(ATTRIBUTE_SEPARATOR)
+        ))),
+        os.linesep).encode()
 
 
 def round_score(score, decimal_points=8):
@@ -48,7 +54,43 @@ def round_score(score, decimal_points=8):
     return float(round(score, decimal_points))
 
 
-if __name__ == '__main__':
+def get_months_to_import(dates, date_format):
+
+    def get_dates(initial_date, final_date):
+        return [
+            date_.strftime(date_format) for date_ in rrule(MONTHLY, dtstart=initial_date, until=final_date)
+        ]
+
+    if dates == 'all':
+        initial_date = datetime.strptime('201805', date_format)
+        final_date = datetime.now()
+
+        return get_dates(initial_date, final_date)
+
+    all_dates = dates.split(',')
+    months_to_import = list(filter(lambda date: '-' not in date, all_dates))
+    range_dates = filter(lambda date: '-' in date, all_dates)
+    for date_range in range_dates:
+        lower_range, upper_range = date_range.split('-')
+        initial_date = datetime.strptime(lower_range, date_format)
+        final_date = datetime.strptime(upper_range, date_format)
+        months_to_import += get_dates(initial_date, final_date)
+
+    return months_to_import
+
+
+@click.command(name='Process file')
+@click.option('--workers', '-w', type=int, default=NUMBER_OF_WORKERS, help=f'Number of works to process file. Defaults: {NUMBER_OF_WORKERS} - total of CPUS on the machine')
+@click.option('--compression-level', '-c', type=int, default=GZIP_COMPRESSION_LEVEL, help=f'Gzip compression level (0-9) defaults: {GZIP_COMPRESSION_LEVEL}')
+@click.option('--dates', default='all', type=str, help='Date to migrate in the "YYYYMM" format. Multiple date separated by "," or range of dates separated by "-"')
+@click.option('--date-format', default=DATE_FORMAT, type=str, help=f'Format in which dates will be input and searched for. Should be compatible with Python datetime format. Default {DATE_FORMAT}. ')
+def process(workers, compression_level, dates, date_format):
+
+    files = []
+    months_to_import = get_months_to_import(dates=dates, date_format=date_format)
+    for month in months_to_import:
+        pattern = os.path.join(ORIGINALS_PATH, 'configs', month, '*.csv.gz')
+        files += glob.glob(pattern)
 
     required_folders = [
         ORIGINALS_PATH, ZIPPED_PATH, PARSED_PATH
@@ -58,7 +100,6 @@ if __name__ == '__main__':
         if not os.path.isdir(folder):
             os.makedirs(folder)
 
-    files = glob(os.path.join(ORIGINALS_PATH, 'configs', '201805', '*.csv.gz'))
     t1 = time()
     for config_file in files:
         print(f'Parsing file: {config_file}')
@@ -66,11 +107,15 @@ if __name__ == '__main__':
 
         parsed_file_path = os.path.join(PARSED_PATH, filename)
         p1 = time()
-        with gzip.GzipFile(parsed_file_path, 'w', compresslevel=GZIP_COMPRESSION_LEVEL) as parsed_file:
-            parsed_lines = parse_file(config_file)
+        with gzip.GzipFile(parsed_file_path, 'w', compresslevel=compression_level) as parsed_file:
+            parsed_lines = parse_file(config_file, workers=workers)
             parsed_file.writelines(parsed_lines)
         p2 = time()
         print(f'Parse complete in {p2 - p1:.2f}s.')
 
     print(f'Total files parsed: {len(files)}')
     print(f'Elapsed time: {time()-t1:.2f}s')
+
+
+if __name__ == '__main__':
+    process()
--- s3_experiments.py
+++ s3_experiments.py
@@ -4,14 +4,18 @@ from time import time
 
 from boto3 import Session
 
-from convert_file import ORIGINALS_PATH
+from convert_file import ORIGINALS_PATH, PARSED_PATH, DATE_FORMAT, extract_file_name, get_months_to_import
 
+import glob
+
+import click
 
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 region_name = os.getenv('REGION_NAME')
 profile_name = os.getenv('AWS_PROFILE_NAME')
 sk_bucket_name = os.getenv('SK_BUCKET_NAME')
+sk_destination_bucket = '3ebdg67-dwh'
 
 session = Session(
     # aws_secret_access_key=aws_secret_access_key,
@@ -35,18 +39,52 @@ def download_file(item, destination_folder, s3_client):
     # print('finilising processing {}:'.format(key))
 
 
-if __name__ == '__main__':
-    client = session.client('s3')
+def upload_file(file_path, s3_client):
+    destination_bucket = '3ebdg67-dwh'
+    filename = extract_file_name(file_path)
 
-    months_to_import = [
-        '201805', '201806', '201807', '201808', '201809', '201810', '201811', '201812',
-        '201901', '201902', '201903', '201904', '201905', '201906', '201907', '201908', '201909', '201910', '201911', '201912',
-        '202001', '202002', '202003', '202004']
+    prefix = 'search_keeper_dev/configs/'
+    key = '{}{}'.format(prefix, filename)
+    print(f'Uploading {key}')
+    s3_client.upload_file(Filename=file_path, Bucket=destination_bucket, Key=key)
+    print(f'{key} uploaded successfully')
 
+
+@click.group(name='s3')
+def s3_manager():
+    pass
+
+
+@click.command(name='upload')
+@click.option('--dates', default='all', type=str, help='Date to migrate in the "YYYYMM" format. Multiple date separated by "," or range of dates separated by "-"')
+def push_to_s3(dates):
+    click.echo("Entering upload to s3")
+    months_to_import = get_months_to_import(dates, DATE_FORMAT)
+
+    files = []
+    for month in months_to_import:
+        pattern = f'{PARSED_PATH}/*{month}*'
+        files += glob.glob(pattern)
+
+    print(f'Uploading {len(files)} files for range {dates}')
+    inputs = [[file_, client] for file_ in files]
+    t1 = time()
+    pool = ThreadPool()
+    pool.starmap(upload_file, inputs)
+    t2 = time()
+    print(f'Uploaded finished in {t2-t1:.2f}s')
+
+
+@click.command(name='download')
+@click.option('--dates', default='all', type=str, help='Date to migrate in the "YYYYMM" format. Multiple date separated by "," or range of dates separated by "-"')
+def download_from_s3(dates: str):
+    click.echo("Entering download from s3")
+    click.echo(f'Downloading "{dates}" files')
+
+    months_to_import = get_months_to_import(dates, DATE_FORMAT)
     total_files = 0
     initial_time = time()
     for month in months_to_import:
-        print('Downloading {} month'.format(month))
         prefix = 'search_keeper/configs/{}'.format(month)
         items = client.list_objects_v2(Bucket=sk_bucket_name, Prefix=prefix)
 
@@ -56,15 +94,28 @@ if __name__ == '__main__':
         if not os.path.isdir(destination_folder):
             print("Creating destination folder: {}".format(destination_folder))
             os.makedirs(destination_folder)
-        total_files += len(items['Contents'])
+        total_files_per_month = len(items['Contents'])
+        total_files += total_files_per_month
         inputs = [[content, destination_folder, client] for content in items['Contents']]
+
+        print(f'Downloading {total_files_per_month} files for {month}')
         total_processes = 4
         t1 = time()
         pool = ThreadPool(total_processes)
-        results = pool.starmap(download_file, inputs)
+        pool.starmap(download_file, inputs)
         pool.close()
         t2 = time()
 
-        print("{} items downladed for '{}' in {:.2f} s".format(len(items['Contents']), month, (t2 - t1)))
+        print("Items downladed in {:.2f} s".format(t2 - t1))
     final_time = time()
-    print('{} items downloaded in {:.2f} s'.format(total_files, (final_time - initial_time)))
+    print('A total of {} items downloaded in {:.2f} s'.format(total_files, (final_time - initial_time)))
+
+
+if __name__ == '__main__':
+    client = session.client('s3')
+
+    s3_manager.add_command(push_to_s3)
+    s3_manager.add_command(download_from_s3)
+
+    s3_manager()
+
